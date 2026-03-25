97ea437acb14797ca42d7f95ff46d02ec8048c25
Shaury Saxena <shaurydeepsaxena@gmail.com>
2026-03-25 18:01:15 +0530

    key

--- expenses/views.py
+++ expenses/views.py
@@ -417,7 +417,7 @@ Return JSON strictly in the following structure:
                 "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent",
                 headers={
                     "Content-Type": "application/json",
-                    "x-goog-api-key": "AIzaSyCyY9-6qIJsKJ52ucfoGOmzVPUHKjThZlc"
+                    "x-goog-api-key": ""
                 },
                 data=json.dumps(request_body)
             )
