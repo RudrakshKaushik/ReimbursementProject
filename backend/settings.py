18d3f77447ef6453e1801754ac6852cf798da3ac
Tohoto <tohoto@Tohotos-MacBook-Pro.local>
2026-06-17 13:27:42 +0530

    test deployment

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,6 @@
 import os
+
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -64,14 +66,9 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # ✅ DATABASE
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 # ✅ PASSWORD VALIDATORS
